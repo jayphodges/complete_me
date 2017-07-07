60eb43af365fcf0740e72edda6de6061795ab1b0
jayphodges <jayphodges@gmail.com>
2017-07-07 13:44:38 -0600

    Add structure to node and complete_me

---
+++ lib/complete_me.rb
@@ -0,0 +1,17 @@
+# /lib/complete_me.rb
+
+class CompleteMe
+
+  def insert(word)
+
+  end
+
+  def populate(several_arguements)
+
+  end
+
+  def insert_words(several_arguements)
+
+  end
+
+end
---
+++ lib/node.rb
@@ -0,0 +1,8 @@
+# /lib/node.rb
+class Node
+  attr_accessor :complete, :a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l, :m, :n, :o, :p, :q, :r, :s, :t, :u, :v, :w, :x, :y, :z
+  def initialize(score, title)
+    @complete = false
+    @a, @b, @c, @d, @e, @f, @g, @h, @i, @j, @k, @l, @m, @n, @o, @p, @q, @r, @s, @t, @u, @v, @w, @x, @y, @z = nil
+  end
+end
